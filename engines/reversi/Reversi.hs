81902fba47dfa251a7be0088608f179461115b5f
Laurie Clark-Michalek <bluepeppers@archlinux.us>
2012-06-27 00:28:36 +0100

    Rewrote reversi engine using new two player board game library

--- engines/reversi/Reversi.hs
+++ engines/reversi/Reversi.hs
@@ -1,9 +1,13 @@
 module Main where
 
+import HG.Engines.TwoPlayerBoard
+
 import Debug.Trace (trace)
 
 import Control.Monad (liftM)
-import Data.Maybe (isNothing)
+import Data.Maybe (isNothing, catMaybes)
+import Data.List(maximumBy, group, sort)
+import Data.Ord(comparing)
 
 import System.Environment (getArgs)
 import System.Exit (exitFailure, exitSuccess)
@@ -12,36 +16,59 @@ import System.Random (randomR, random, getStdRandom)
 import Text.JSON
 import HG.Proto
 
-playerCount = 2
 boardSize = 8
 
-data Config = Config { listenPort :: Integer
-                     , gameID :: Integer
-                     }
+impl :: GameImpl
+impl = GameImpl { gameWon = gameWon'
+                , applyMove = applyMove'
+                , playerCount = 2
+                , createGame = createGame'
+                , chooseStarter = chooseStarter'
+                }
 
-data Player = Player { playerSock :: Handle
-                     , playerID :: PlayerID
-                     }
-              deriving (Eq, Show)
+createGame' :: [Player] -> Player -> Game
+createGame' ps p = Game { gameBoard = setupBoard ps (createSquareBoard 8)
+                        , gamePlayers  = ps
+                        , currentPlayer = p
+                        , gameStatus = Playing
+                        }
 
-data Game = Game { gameBoard :: Board
-                 , gamePlayers :: [Player]
-                 , currentPlayer :: Player
-                 , gameTurns :: Integer
-                 , gameStatus :: GameStatus}
-          deriving (Eq, Show)
+chooseStarter' :: [Player] -> IO Player
+chooseStarter' players = fmap (players !!) chosen
+  where chosen = getStdRandom (randomR (0, length players - 1))
 
-data GameStatus = Playing
-                | Finished { winner :: Maybe Player
-                           , reason :: FinishReason }
-                deriving (Eq, Show)
+applyMove' :: Game -> Move -> Game
+applyMove' g m = case moveTo m of
+  Nothing -> finishGame g (Just $ notCurrentPlayer g) BadMessage
+  Just move -> if validMove move (currentPlayer g) (gameBoard g)
+               then doMove move g
+               else finishGame g (Just $ notCurrentPlayer g) BadMove
 
-data FinishReason = Stalemate
-                  | Disconnect
-                  | PlayerWin
-                  | BadMessage
-                  | BadMove
-                  deriving (Eq, Show)
+executeMove :: Coord -> Game -> Game
+executeMove c g
+  | validMove c (currentPlayer g) (gameBoard g) = doMove c g
+  | otherwise = finishGame g (Just $ notCurrentPlayer g) BadMove
+
+gameWon' :: Game -> Maybe Player
+gameWon' game
+  | fullBoard game = mostPlaced game
+  | otherwise = Nothing
+
+fullBoard game = (filter (/= []) cols) == []
+  where cols = map (filter (==Nothing)) (gameBoard game)
+
+mostPlaced game = findPlayer (gamePlayers game) winner
+  where vals = catMaybes $ concat (gameBoard game)
+        winner = mostCommon vals
+
+findPlayer :: [Player] -> PlayerID -> Maybe Player
+findPlayer (p:ps) pid
+  | (playerID p) == pid = Just p
+  | otherwise = findPlayer ps pid
+findPlayer _ _ = Nothing
+
+mostCommon :: Ord a => [a] -> a
+mostCommon = head . maximumBy (comparing length) . group . sort
 
 data Direction = N | NE | E | SE | S | SW | W | NW
                deriving (Eq, Show)
@@ -56,7 +83,6 @@ directionModifiers = [ (N, \(x, y) -> (x, y+1))
                      , (W, \(x, y) -> (x-1, y))
                      , (NW, \(x, y) -> (x-1, y+1))
                      ]
-
 
 onVertical :: Coord -> Coord -> Bool
 onVertical (x1, _) (x2, _) = x1 == x2
@@ -85,15 +111,6 @@ statusToInteger (Finished _ Stalemate) = 2
 statusToInteger (Finished _ Disconnect) = 3
 statusToInteger (Finished _ BadMessage) = 4
 statusToInteger (Finished _ BadMove) = 5
-
-type Board = [[Maybe PlayerID]]
-
-addToBoard :: Coord -> PlayerID -> Board -> Board
-addToBoard (x, y) pid b = brow ++ [bcol ++ [Just pid] ++ acol] ++ arow
-  where (brow, (row:arow)) = splitAt (fromIntegral x - 1) b
-        (bcol, (col:acol)) = splitAt (fromIntegral y - 1) row
-
-getFromBoard (x, y) b = (b !! fromInteger (x - 1)) !! fromInteger (y - 1)
 
 -- Can the given player place a stone here
 validMove :: Coord -> Player -> Board -> Bool
@@ -136,9 +153,6 @@ swapStones c p d b = swapStones' (changer c) (addToBoard c (playerID p) b)
           where stone = getFromBoard c b
         changer = snd (head (filter (\p -> fst p == d) directionModifiers))
 
-liftBoard :: (Board -> Board) -> Game -> Game
-liftBoard f g = g {gameBoard = f (gameBoard g)}
-
 gameEnded :: Game -> Bool
 gameEnded g = all not (concat validOpenPlaceMap)
   where validOpenPlaceMap = mapBoardC (\ c v -> isNothing v && openPlace c g)
@@ -148,15 +162,10 @@ playerCanMove :: Player -> Board -> Bool
 playerCanMove p b = or (concat validPlaceMap)
   where validPlaceMap = mapBoardC (\c v-> isNothing v && validMove c p b) b
 
-type Coord = (Integer, Integer)
-
-type PlayerID = Integer
-
 doMove :: Coord -> Game -> Game
 doMove c g
   | gameEnded g' = declareWinner g'
-  | playerCanMove np b' = g' { currentPlayer = np
-                             , gameTurns = gameTurns g + 1}
+  | playerCanMove np b' = g' { currentPlayer = np}
   where g' = liftBoard (updateBoard c p) g
         np = notCurrentPlayer g
         p = currentPlayer g
@@ -171,51 +180,13 @@ declareWinner g = g {gameStatus = gs}
 updateBoard :: Coord -> Player -> Board -> Board
 updateBoard c p b = foldl (flip (swapStones c p)) b directions
 
--- Reads a move in the forma
+-- Reads a move in the form
 readMove :: (JSObject (Integer, Integer)) -> Maybe Coord
 readMove jsobj
   | length vals /= 1 ||
     (fst . head $ vals) /= "placed" = Nothing
   | otherwise = Just . snd . head $ vals
   where vals = fromJSObject jsobj
-
-receiveMove :: Player -> IO (Maybe Coord)
-receiveMove p = liftM (>>= readMove)
-                (receiveMessage $ playerSock p)
-
-updatePlayers :: Game -> Game -> IO Game
-updatePlayers g g' = do
-  let b = gameBoard g
-      b' = gameBoard g'
-      diff = fst $ diffBoards (currentPlayer g) b b'
-      message = generateUpdateMessage diff g g'
-  mapM_ (\p -> sendMessage (playerSock p) message)
-    (gamePlayers g)
-  return g'
-
-generateUpdateMessage :: [Coord] -> Game -> Game -> JSObject JSValue
-generateUpdateMessage diff g g' = toJSObject
-  [ ("changed", showJSON diff)
-  , ("by", showJSON (playerID (currentPlayer g)))
-  , ("next_turn", showJSON (playerID (currentPlayer g')))
-  , ("game_status", showJSON (statusToInteger (gameStatus g')))
-  ]
-
-diffBoards :: Player -> Board -> Board -> ([Coord], PlayerID)
-diffBoards p b b' = (changed, playerID p) -- At least one stone
-                  -- must always changed
-  where changed = foldl (\cs (c, v) -> if v /= -1
-                                       then c:cs
-                                       else cs) [] coords
-        coords = concat $ mapBoardC worker b'
-        worker c (Just v) = if getFromBoard c b /= Just v
-                            then (c, v)
-                            else (c, -1)
-        worker c Nothing = (c, -1)
-
--- A blank board
-newBoard :: Board
-newBoard = replicate 8 (replicate 8 Nothing)
 
 placeStone :: Coord -> PlayerID -> Board -> Board
 placeStone = addToBoard
@@ -233,75 +204,17 @@ setupBoard players b = placedSecond
         placedFirst = placeStones b [(4, 4), (5, 5)] firstPid
         placedSecond = placeStones placedFirst [(4, 5), (5, 4)] secondPid
 
-newGame :: [Player] -> Player -> Game
-newGame ps c = Game (setupBoard ps newBoard) ps c 0 Playing
-
-serializeGame :: Game -> Player -> JSObject JSValue
-serializeGame g p = toJSObject [ ("board", board)
-                               , ("next_turn", current)
-                               , ("you", you)]
-  where board = showJSON $ mapBoard worker (gameBoard g)
-          where worker (Just v) = v
-                worker Nothing = -1
-        current = showJSON . playerID . currentPlayer $ g
-        you = showJSON . playerID $ p
-
-chooseStarter :: [Player] -> IO Player
-chooseStarter players = fmap (players !!) chosen
-  where chosen = getStdRandom (randomR (0, length players - 1))
-
-notCurrentPlayer :: Game -> Player
-notCurrentPlayer g = head $
-                     dropWhile (== currentPlayer g) (cycle (gamePlayers g))
-
 cyclePlayer :: Game -> Game
 cyclePlayer g = g { currentPlayer = notCurrentPlayer g }
 
 changeStatus :: Game -> GameStatus -> Game
 changeStatus g status = g {gameStatus = status}
 
-fullSync :: Game -> [Player] -> IO Game
-fullSync g ps = mapM_ (\p -> sendMessage (playerSock p) (serializeGame g p)) ps
-                >> return g
-
-startGame :: [Player] -> IO Game
-startGame players = do
-  starter <- chooseStarter players
-  let game = newGame players starter
-  game' <- fullSync game players
-  putStrLn "Completed full gamestate sync"
-  gameLoop game'
-
-finishGame :: Game -> Maybe Player -> FinishReason -> Game
-finishGame g p r = g {gameStatus = Finished p r}
-
-gameLoop :: Game -> IO Game
-gameLoop g
-  | gameStatus g /= Playing = return g
-  | otherwise = do
-    mmove <- receiveMove (currentPlayer g)
-    putStrLn $ "Recived move from " ++ (show . playerID . currentPlayer $ g)
-    case mmove of
-      Nothing -> return $ finishGame g (Just $ notCurrentPlayer g) BadMessage
-      Just coord -> updatePlayers g g' >>= gameLoop
-        where g' = executeMove coord g
-
-executeMove :: Coord -> Game -> Game
-executeMove c g
-  | validMove c (currentPlayer g) (gameBoard g) = doMove c g
-  | otherwise = finishGame g (Just $ notCurrentPlayer g) BadMove
-
 main :: IO()
 main = withSocketsDo $ do
   config <- getArgs >>= parseArgs
-  cons <- reciveConnections playerCount (listenPort config)
-  let players = map (\x -> Player (snd x) (fst x)) cons
-  putStrLn $ "Recived connections, " ++ show (length $ players) ++
-    " players: " ++ (show $ map playerID players)
-  startGame players
-  cleanupConnections $ map playerSock players
-  return ()
+  runGame impl config
 
 parseArgs :: [String] -> IO Config
-parseArgs (port:gameID:[]) = return $ Config (read port) (read gameID)
+parseArgs (port:[]) = return $ Config (read port)
 parseArgs _ = putStrLn "Bad arguments" >> exitFailure
